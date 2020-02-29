7e34940c9278219358dd70c46daeed28e33c8a2d
Yevgeny Levin <devlev1980@gmail.com>
2020-02-29 19:43:02 +0200

    adding project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-import { DialogModule } from 'primeng/dialog';
 
 import { AppComponent } from './app.component';
+import {PrimengModule} from './shared/primeng.module';
+import {AppRoutingModule} from './app-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+import {InputsComponent} from './inputs/inputs.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ButtonsComponent,
+        InputsComponent,
+        LayoutComponent,
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
-        TableModule,
         HttpClientModule,
-        InputTextModule,
-        DialogModule,
-        ButtonModule
+        PrimengModule,
+        AppRoutingModule
+
     ],
     providers: [],
     bootstrap: [AppComponent]
